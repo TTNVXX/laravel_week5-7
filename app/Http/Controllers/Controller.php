81983ba14a41d638bea657108c398fed4e17335f
TTNVXX <tegarfadillahz1@gmail.com>
2024-04-18 22:43:35 +0700

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+// Tegar Fadillah Zanestri - 1304211020
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
---
+++ resources/views/v_latihan1.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- // Tegar Fadillah Zanestri - 1304211020 -->
+<html>
+	<head>
+        <title>Latihan 1</title>
+    </head>
+	<body>
+		Nama : {{$nama}} <br/>
+		Asal : {{$asal}} <br/>
+	</body>
+</html>
